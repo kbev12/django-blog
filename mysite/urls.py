283f01e90f69ee3e15b58fa58aea0b67d809c917
kbev12 <110877101+kbev12@users.noreply.github.com>
2024-02-03 22:59:17 -0500

    added views

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include  # <-- Make sure you have both of these imports.
 
+
 urlpatterns = [
     path('polling/', include('polling.urls')),  # <-- Add this
     path('admin/', admin.site.urls),
+    path('', include('blogging.urls')),
 ]
